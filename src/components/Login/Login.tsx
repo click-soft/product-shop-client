bbc786804aded29a439eb22a51832378fc467c23
Kaburi <specialemail90@gmail.com>
2023-10-11 17:25:04 +0900

    test

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -9,6 +9,7 @@ import LoginMsgCard from './LoginMsgCard/LoginMsgCard';
 import { login } from '../../graphql/mutates/auth';
 import useToast from '../../hooks/use-toast';
 import { getUser } from '../../graphql/queries/user';
+import cookies from '../../utils/cookies';
 
 function Login() {
   const navigate = useNavigate();
@@ -29,9 +30,11 @@ function Login() {
     if (!error) {
       try {
         const result = await login(isBuisness, inputValue);
-        if (result.message === 'success') {
-          navigate('/');
-        }
+        const user: string = cookies.get('user');
+        showToast('error', user);
+        // if (result.message === 'success') {
+        //   navigate('/');
+        // }
       } catch (err: any) {
         showToast('error', err.message);
       }
