2cb0622155f9e33a3786f204e7cf2e467fd4a39f
Kaburi <specialemail90@gmail.com>
2023-11-24 09:49:14 +0900

    상품 이미지 추가 작업 진행

--- src/components/Main/ProductItem/ProductItem.tsx
+++ src/components/Main/ProductItem/ProductItem.tsx
@@ -1,3 +1,4 @@
+import useGetLoginedUser from '@/hooks/use-get-logined-user';
 import ProductListSub from '../../../interfaces/product-list-sub';
 import useModalStore from '../../../store/modal.store';
 import styles from './ProductItem.module.scss';
@@ -8,22 +9,22 @@ interface ProductItemProps {
 }
 const ProductItem: React.FC<ProductItemProps> = (props) => {
   const { showProduct } = useModalStore();
+  const user = useGetLoginedUser();
   const pls = props.productList;
   const itemClickHandler = () => {
     showProduct({ code: props.productCode, data: pls });
   };
+  const url = import.meta.env.VITE_BACKEND_URL;
+  const imageUrl = `${url}/images/productlist_image/${user?.jisa}/${pls.smCode}`;
 
+  // console.log(`${pls.smMyung} / ${pls.smCode}`);
+  
   return (
     <li className={styles.item} onClick={itemClickHandler}>
-      <section>
-        <div className={styles.item__name}>{pls.smMyung}</div>
-      </section>
-      <section>
-        <div className={styles.item__unit}>{pls.danwi}</div>
-      </section>
-      <section>
-        <div className={styles.cost}>{pls.danga.toLocaleString()}원</div>
-      </section>
+      <img className={styles.item__image} src={imageUrl} alt="상품 이미지" />
+      <div className={styles.item__name}>{pls.smMyung}</div>
+      <div className={styles.item__unit}>{pls.danwi}</div>
+      <div className={styles.cost}>{pls.danga.toLocaleString()}원</div>
     </li>
   );
 };
