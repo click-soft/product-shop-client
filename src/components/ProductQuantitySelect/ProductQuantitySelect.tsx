5c62ad612846a9e3c9259859f338e839a4fec7be
Kaburi <specialemail90@gmail.com>
2023-11-28 19:13:58 +0900

    .

--- src/components/ProductQuantitySelect/ProductQuantitySelect.tsx
+++ src/components/ProductQuantitySelect/ProductQuantitySelect.tsx
@@ -5,12 +5,10 @@ interface ProductQuantitySelectProps {
   onChange: (value: number) => void;
   value: number;
   isFit: boolean;
-  step : number;
+  step: number;
 }
 const ProductQuantitySelect: React.FC<ProductQuantitySelectProps> = (props) => {
-  return (
-    <IntUpAndDown value={props.value} step={props.step} min={props.step} onChange={(v) => props.onChange(v)} />
-     );
+  return <IntUpAndDown value={props.value} step={props.step} min={props.step} onChange={(v) => props.onChange(v)} />;
 };
 
 export default ProductQuantitySelect;
